c85a84480111e46142f4dc7a4e09da5212fe0ae2
Yahor <manforprom@gmail.com>
2022-03-22 21:21:01 +0300

    create adapter

--- app/src/main/java/com/example/chaykuapp/AllTeaTabFragment.kt
+++ app/src/main/java/com/example/chaykuapp/AllTeaTabFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.chaykuapp.databinding.FragmentFirstBinding
+import com.example.chaykuapp.databinding.AllTeaTabFragmentBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class AllTeaTabFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: AllTeaTabFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = AllTeaTabFragmentBinding.inflate(inflater,container,false)
         return binding.root
 
     }
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.addAnotherTea.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
---
+++ app/src/main/java/com/example/chaykuapp/adapters/AllTeaTabRecyclerViewAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.chaykuapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chaykuapp.data.UsersTea
+import com.example.chaykuapp.databinding.AllTeaRecyclerItemBinding
+
+class AllTeaTabRecyclerViewAdapter() : ListAdapter<UsersTea,
+        AllTeaTabRecyclerViewAdapter.ViewHolder>(TeaItemCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            AllTeaRecyclerItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: AllTeaTabRecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(binding: AllTeaRecyclerItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(tea: UsersTea){
+
+        }
+    }
+
+}
+
+private class TeaItemCallBack : DiffUtil.ItemCallback<UsersTea>(){
+    override fun areItemsTheSame(oldItem: UsersTea, newItem: UsersTea): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: UsersTea, newItem: UsersTea): Boolean {
+       return oldItem.teaName == newItem.teaName
+    }
+
+}

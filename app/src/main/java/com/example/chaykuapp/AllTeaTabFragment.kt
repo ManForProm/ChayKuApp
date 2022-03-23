48dd2982802755eaf67bbfc4eafd20d9e24fa6e3
Yahor <manforprom@gmail.com>
2022-03-23 22:15:58 +0300

    little fixes

--- app/src/main/java/com/example/chaykuapp/AllTeaTabFragment.kt
+++ app/src/main/java/com/example/chaykuapp/AllTeaTabFragment.kt
@@ -6,39 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chaykuapp.adapters.AllTeaTabRecyclerViewAdapter
+import com.example.chaykuapp.data.UsersTea
 import com.example.chaykuapp.databinding.AllTeaTabFragmentBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class AllTeaTabFragment : Fragment() {
+class AllTeaTabFragment : Fragment(R.layout.all_tea_tab_fragment) {
+    private var adapter:AllTeaTabRecyclerViewAdapter? = null
 
-    private var _binding: AllTeaTabFragmentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = AllTeaTabFragmentBinding.inflate(inflater,container,false)
-        return binding.root
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val allTeaAdapter = AllTeaTabRecyclerViewAdapter()
+
+        this.adapter = allTeaAdapter
+
+        val binding = AllTeaTabFragmentBinding.bind(view)
+
+        adapter?.submitList(listOf(UsersTea(
+                "Oolong",
+            "Sasha",
+            "black",
+            "rose",
+            "none",
+            "none"
+        )))
+
+        with(binding.allTeaList){
+            layoutManager = LinearLayoutManager(context)
+            this.adapter =allTeaAdapter
+        }
+
         binding.addAnotherTea.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        adapter = null
     }
 }
